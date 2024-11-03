4b36b30fef2d72f4850cb12ff063750842d09d2a
taikonglaji3 <1322925214@qq.com>
2024-11-03 20:39:21 +0800

    Update 7z解压.py

--- 7z解压.py
+++ 7z解压.py
@@ -9,20 +9,13 @@ def jy7z():
     j7z=Tk()
     j7z.geometry('300x180')
     j7z.title('7z解压')
-    baolujing=Label(j7z,text='压缩包路径文本txt：')
-    shurubao=Entry(j7z)
+    baolujing,jielujing,password=Label(j7z,text='压缩包路径文本txt：'),Label(j7z,text='解压至目录：'),Label(j7z,text='密码：')
+    shurubao,shurujie,shurumi=Entry(j7z),Entry(j7z),Entry(j7z)
     queren=Button(j7z,text='开始解压',command=queren7z)
-    jielujing=Label(j7z,text='解压至目录：')
-    shurujie=Entry(j7z)
-    password=Label(j7z,text='密码：')
-    shurumi=Entry(j7z)
-    baolujing.pack()
-    shurubao.pack()
-    jielujing.pack()
-    shurujie.pack()
-    password.pack()
-    shurumi.pack()
-    queren.pack()
+
+    sign1=[baolujing,shurubao,jielujing,shurujie,password,shurumi,queren]
+    for i in sign1:
+        i.pack()
     j7z.mainloop()
 
 def queren7z():
@@ -34,18 +27,19 @@ def queren7z():
     for k, v in path_dict.items():
         path = k.replace('"', '')
         extract = mulu + str(v)
-        with SevenZipFile(path,password=shurumi.get()) as archive:
-            archive.extractall(path=extract)
-
+        if path[-3,-1]=='.7z':
+            with SevenZipFile(path,password=shurumi.get()) as archive:
+                archive.extractall(path=extract)
+        if path[-7,-1]=='.7z.001':
+            with
 
 jieya=Tk()
 jieya.geometry('300x120')
 jieya.title('解压方式')
 label=Label(jieya,text='请选择解压方式')
 label.pack()
-jieya7z=Button(jieya,text='7z解压',command=jy7z)
-jieyazip=Button(jieya,text='zip解压')
-jieyarar=Button(jieya,text='rar解压')
+jieya7z,jieyazip,jieyarar=Button(jieya,text='7z解压',command=jy7z),Button(jieya,text='zip解压'),Button(jieya,text='rar解压')
+
 jieya7z.pack(fill=X,expand=True)
 jieyazip.pack(fill=X,expand=True)
 jieyarar.pack(fill=X,expand=True)
